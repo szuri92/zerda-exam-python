cbce80bec60adb2ec9cf81e7416aff61cc519937
Máté Szurovecz <szuri92@gmail.com>
2016-12-05 09:44:31 +0100

    first.py first solution

--- first.py
+++ first.py
@@ -3,4 +3,15 @@
 # It should raise an error if the parameter is not a list.
 # Example: with the input [1, 2, 3, 4, 5] it should return [2, 4].
 
-#hali
+
+def even_list(my_list):
+    result = []
+    if type(my_list) == list:
+        for i in range(len(my_list)):
+            if i % 2 == 1:
+                result.append(my_list[i])
+        return result
+    else:
+        raise TypeError
+
+even_list(3)
