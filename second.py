bc2ad1df941377ea8bf885eb806eb58f86af66af
Máté Szurovecz <szuri92@gmail.com>
2016-12-05 09:46:25 +0100

    add a solution for second.py

--- second.py
+++ second.py
@@ -4,3 +4,16 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+def write_in_text(file_name, text):
+    try:
+        my_file = open(file_name, 'a')
+        for i in range(0, 10):
+            my_file.write(text)
+        my_file.close()
+        return True
+    except:
+        return False    
+
+write_in_text('tree.txt', 'apple')
+print(write_in_text('tree.txt', 'apple'))
