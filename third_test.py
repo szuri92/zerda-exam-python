365eb3a06d106429e230d489998261f88bfd709e
Máté Szurovecz <szuri92@gmail.com>
2016-12-05 11:56:59 +0100

    add new tests

--- third_test.py
+++ third_test.py
@@ -2,10 +2,16 @@ from third import count_letter_in_string
 import unittest
 
 class TestMyFunctions(unittest.TestCase):
+    def test_count_letter_in_string_with_simple_string(self):
+        self.assertEqual(count_letter_in_string('Nem Vagyok Bela!', 'a'), 2)
     def test_count_letter_in_string_with_no_string(self):
         self.assertEqual(count_letter_in_string(2, 'b'), 0)
     def test_count_letter_in_string_with_letter_as_number(self):
         self.assertEqual(count_letter_in_string('alma5', 5), 0)
+    def test_count_letter_in_string_with_empty_string(self):
+        self.assertEqual(count_letter_in_string('', 'a'), 0)
+    def test_count_letter_in_string_with_letter_as_string(self):
+        self.assertEqual(count_letter_in_string('alma', 'alma'), 0)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
